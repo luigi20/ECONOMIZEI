e06d64a0e9bc7bf14035b5ef296d4acacdd7058a
luigi20 <luisopentec@gmail.com>
2019-10-18 22:47:31 -0300

    Template e index

---
+++ resources/views/Home/Home.blade.php
@@ -0,0 +1,26 @@
+@extends('template.principal')
+@section('conteudo')
+<div class="card text-white bg-danger mb-3" style="max-width: 15rem; margin-left:-300px;margin-top:-200px,">
+        <div class="card-header">DESPESA </div>
+        <div class="card-body">
+          <h5 class="card-title">R$ 450,00</h5>
+          <p class="card-text">MÊS ATUAL</div>
+</div>  
+
+<div class="card text-white bg-info mb-3" style="max-width: 15rem;margin-left:-30px;margin-top:-160px">
+        <div class="card-header">RECEITA</div>
+        <div class="card-body">
+          <h5 class="card-title">R$ 2000,00</h5>
+          <p class="card-text">MÊS ATUAL</p>
+        </div>
+      </div> 
+
+      <div class="card text-white bg-success mb-3" style="max-width: 15rem;margin-left:240px;margin-top:-160px">
+        <div class="card-header">Saldo Total</div>
+        <div class="card-body">
+          <h5 class="card-title">R$ 1600,00</h5>
+          <p class="card-text">MÊS ATUAL</p>
+        </div>
+      </div> 
+      
+@endsection
